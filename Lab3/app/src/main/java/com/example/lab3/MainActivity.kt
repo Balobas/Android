059c7b0cfaeeaa2971e895c40aba0f9e7a875406
Владислав Балобанов <balobasta@gmail.com>
2020-10-12 16:53:43 +0200

    Added main layout and name validation to Lab3

--- Lab3/app/src/main/java/com/example/lab3/MainActivity.kt
+++ Lab3/app/src/main/java/com/example/lab3/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.lab3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        make_photo_btn.setOnClickListener { if (validateName()) openCamera()}
+    }
+
+    private fun validateName(): Boolean {
+        return name_input.text.isNotEmpty()
+    }
+
+    private fun openCamera() {
+
     }
 }
