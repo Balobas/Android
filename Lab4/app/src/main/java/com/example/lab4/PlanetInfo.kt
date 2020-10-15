6ac5aba4ac9b9abea05eeb96f487dcb0b9ea4217
Владислав Балобанов <balobasta@gmail.com>
2020-10-15 17:06:32 +0200

    Lab 4 : Added class PlanetInfo

---
+++ Lab4/app/src/main/java/com/example/lab4/PlanetInfo.kt
@@ -0,0 +1,6 @@
+package com.example.lab4
+
+import android.graphics.Bitmap
+
+class PlanetInfo(val name : String, val image : Bitmap, val distanceToSun : Double, val hasLive : Boolean, val link : String) {
+}
